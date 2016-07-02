2d3884d6b9a6a117b8f779fd30f0bd5b9354e5dd
Gabriel Lundberg <git@leirb.ag>
2016-07-02 17:01:49 +0800

    Proper figures and quicker rendering.

--- main.js
+++ main.js
@@ -21,50 +21,113 @@ function init() {
 
   state = {
     lastMouse: {},
-    lastPos: {x: 0, y: 0},
-    secondaryPos: {x: 0, y: 0},
-    secondaryVel: {x: 0, y: 0}
+    lastPos: {x: screenElement.width/2, y: screenElement.height/2},
+    //origin: {x: screenElement.width/2, y: screenElement.height/2},
+    primaryPos: {x: screenElement.width/2, y: screenElement.height/2},
+    primaryVel: {x: 0, y: 0},
+    secondaryPos: {x: screenElement.width/2, y: screenElement.height/2},
+    secondaryVel: {x: 0, y: 0},
+    steps: 10
   };
   Object.assign(state.lastMouse, mouse);
 
   window.requestAnimationFrame(render.bind(render, state, mouse, screenElement.getContext("2d"), backElement.getContext("2d")));
 }
 
+function step(state, mouse, origin, dt) {
+  if (mouse.down) {
+    state.primaryVel.x += dt*0.25*(mouse.x - state.primaryPos.x)
+    state.primaryVel.y += dt*0.25*(mouse.y - state.primaryPos.y);
+    state.primaryVel.x -= dt*0.6 * state.primaryVel.x;
+    state.primaryVel.y -= dt*0.6 * state.primaryVel.y;
+  } else {
+    state.primaryVel.x += dt*0.02*(origin.x - state.primaryPos.x)
+    state.primaryVel.y += dt*0.02*(origin.y - state.primaryPos.y);
+    state.primaryVel.x += dt*0.05*(state.secondaryPos.x - state.primaryPos.x)
+    state.primaryVel.y += dt*0.05*(state.secondaryPos.y - state.primaryPos.y);
+    state.primaryVel.x -= dt*0.0001 * state.primaryVel.x;
+    state.primaryVel.y -= dt*0.0001 * state.primaryVel.y; 
+  }
+
+  state.secondaryVel.x += dt*0.05*(state.primaryPos.x - state.secondaryPos.x)
+  state.secondaryVel.y += dt*0.05*(state.primaryPos.y - state.secondaryPos.y);
+  state.secondaryVel.x -= dt*0.0001 * state.secondaryVel.x;
+  state.secondaryVel.y -= dt*0.0001 * state.secondaryVel.y;
+
+
+  state.primaryPos.x += dt*state.primaryVel.x;
+  state.primaryPos.y += dt*state.primaryVel.y;
+  state.secondaryPos.x += dt*state.secondaryVel.x;
+  state.secondaryPos.y += dt*state.secondaryVel.y;
+
+  if (mouse.down) {
+    if (!state.lastMouse.down) {
+      state.primaryVel = {x: 0, y: 0};
+      state.secondaryVel = {x: 0, y: 0};
+      state.primaryPos = {x: mouse.x, y: mouse.y};
+      state.primaryPos = {x: mouse.x, y: mouse.y};
+      state.secondaryPos = {x: mouse.x, y: mouse.y};
+      state.secondaryPos = {x: mouse.x, y: mouse.y};
+    }
+  }
+}
+
 function render(state, mouse, screen, back) {
+  if (mouse.down) {
+    if (!state.lastMouse.down) {
+      back.clearRect(0, 0, back.canvas.width, back.canvas.height);
+      state.steps = 10;
+    }
+  } else {
+    if (state.steps < 1000) {
+      state.steps *= 1.02;
+    }
+  }
 
-  state.secondaryVel.x += 0.05*(mouse.x - state.secondaryPos.x) - 0.01 * state.secondaryVel.x;
-  state.secondaryVel.y += 0.05*(mouse.y - state.secondaryPos.y) - 0.01 * state.secondaryVel.y;
-  state.secondaryPos.x += state.secondaryVel.x;
-  state.secondaryPos.y += state.secondaryVel.y;
+  for (var i = 0; i < state.steps; i++) {
+    step(state, mouse, {x: screen.canvas.width/2, y: screen.canvas.height/2}, 0.1);
 
-  //  screen.clearRect(0, 0, screen.width, screen.height);
-  back.globalAlpha = 0.1;
+    if (!mouse.down) {
+      back.lineCap = "round";
+      back.lineWidth = 1;
+      back.strokeStyle = "rgba(0, 0, 0, 0.2)"
+      back.beginPath();
+      back.moveTo(state.lastPos.x, state.lastPos.y);
+      back.lineTo(state.primaryPos.x, state.primaryPos.y);
+      //back.arc(mouse.x, mouse.y, 5, 0, 2*Math.PI);
+      back.stroke();
+    }
+
+    Object.assign(state.lastPos, state.primaryPos)
+  }
+
+  /*back.globalAlpha = 0.1;
   back.globalCompositeOperation='destination-out';
   back.fillRect(0, 0, back.canvas.width, back.canvas.height)
+  back.globalAlpha = 1.0;
+  back.globalCompositeOperation='source-over';*/
 
-  back.globalAlpha = 1;
-  back.globalCompositeOperation='source-over';
-  back.lineCap = "round";
-  back.lineWidth = 5;
-  back.beginPath();
-  back.moveTo(state.lastPos.x, state.lastPos.y);
-  back.lineTo(state.secondaryPos.x, state.secondaryPos.y);
-  //back.arc(mouse.x, mouse.y, 5, 0, 2*Math.PI);
-  back.stroke();
+  screen.clearRect(0, 0, screen.canvas.width, screen.canvas.height)
 
-  screen.fillStyle = "#fff";
-  screen.fillRect(0, 0, screen.canvas.width, screen.canvas.height)
-  screen.drawImage(back.canvas, 0, 0)
+  if (mouse.down) {
+    screen.fillStyle = "#000";
+    screen.beginPath();
+    screen.arc(mouse.x, mouse.y, 5, 0, 2*Math.PI);
+    screen.fill();
 
-  screen.fillStyle = "#000";
-  screen.beginPath();
-  screen.arc(mouse.x, mouse.y, 5, 0, 2*Math.PI);
-  screen.fill();
+    screen.fillStyle = "#f0f";
+    screen.beginPath();
+    screen.arc(state.secondaryPos.x, state.secondaryPos.y, 5, 0, 2*Math.PI);
+    screen.fill();
 
-  screen.fillStyle = "#f0f";
-  screen.beginPath();
-  screen.arc(state.secondaryPos.x, state.secondaryPos.y, 5, 0, 2*Math.PI);
-  screen.fill();
+    screen.fillStyle = "#0ff";
+    screen.beginPath();
+    screen.arc(state.primaryPos.x, state.primaryPos.y, 5, 0, 2*Math.PI);
+    screen.fill();
+  } else {
+    screen.drawImage(back.canvas, 0, 0)
+  }
+
 
   // mouse just released
   /* if (state.lastMouse.down = true && !mouse.down) {
@@ -76,7 +139,6 @@ function render(state, mouse, screen, back) {
   //back.clearRect(0, 0, back.canvas.width, back.canvas.)
 
   Object.assign(state.lastMouse, mouse)
-  Object.assign(state.lastPos, state.secondaryPos)
   argumentsArray = Array.from(arguments);
   window.requestAnimationFrame(function () { render.apply(render, argumentsArray); });
 }
